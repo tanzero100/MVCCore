d375b9bf0327b1559d737c57f33a636d13ad1833
arvind deo <tanzero100@gmail.com>
2019-05-03 11:28:34 +0530

    Added Project

---
+++ LFDA/LFDA Core Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LFDA_Core_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
